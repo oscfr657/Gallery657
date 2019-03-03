9a4b043b79b820e37e5ad741c6b6feae123ea934
OscFr <oscfr657@gmail.com>
2019-03-03 14:30:38 +0100

    css improvments and small url name update.

--- urls.py
+++ urls.py
@@ -7,5 +7,5 @@ from gallery657.routers import GALLERY657_ROUTER
 
 urlpatterns = [
     url(r'^api/', include(GALLERY657_ROUTER.urls)),
-    url(r'^[\d+]*$', gallery_vue, name='gallery_vue'),
+    url(r'^[\d+]*$', gallery_vue, name='gallery'),
 ]
